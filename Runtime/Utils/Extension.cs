73e677bb4e312a28ab63179d8b49c81bc886245a
Jorge Nahuel Gerones <gerones.jorge@gmail.com>
2025-06-16 20:46:10 -0300

    Minor Extension Update
    -Updated Extension.cs

--- Runtime/Utils/Extension.cs
+++ Runtime/Utils/Extension.cs
@@ -153,5 +153,15 @@ namespace CoreEngine
             return array != null && array.Any();
         }
         #endregion
+
+        #region Array
+        /// <summary>Indicates whether the specified array is null or has a length of zero.</summary>
+        /// <param name="array">The array to test.</param>
+        /// <returns>true if the array parameter is null or has a length of zero; otherwise, false.</returns>
+        public static bool IsNullOrEmpty(this Array array)
+        {
+            return (array == null || array.Length == 0);
+        }
+        #endregion
     }
 }
