d239a91f6ed9943f174d664e26ba6c169d6acfd1
Jorge Nahuel Gerones <gerones.jorge@gmail.com>
2025-09-02 14:55:51 -0300

    Minor DontDestroy Update
    -Updated DontDestroy.cs
    -Updated package.json

--- Runtime/Components/DontDestroy.cs
+++ Runtime/Components/DontDestroy.cs
@@ -7,13 +7,24 @@ namespace CoreEngine
 {
     public class DontDestroy : MonoBehaviour
     {
+        [SerializeField]
+        public bool persistent;
+
         private void Awake()
         {
             DontDestroyOnLoad(gameObject);
         }
 
+        private static List<string> tagsToIgnore = new List<string>();
+
         public static void Clear()
         {
+            //DontDestroy[] contianer = FindObjectsOfType<DontDestroy>();
+            //foreach (DontDestroy cont in contianer)
+            //{
+            //    tagsToIgnore.AddRange(cont.ignore);
+            //}
+
             GameObject temp = new GameObject("Temp_DDOL");
             Object.DontDestroyOnLoad(temp);
 
@@ -23,6 +34,12 @@ namespace CoreEngine
 
             foreach (GameObject obj in rootObjects)
             {
+                //if(tagsToIgnore.Contains(obj.tag)) continue;
+                if (obj.TryGetComponent(out DontDestroy dd))
+                {
+                    if (dd.persistent) continue;
+                }
+                
                 if (obj != temp)
                     Object.Destroy(obj);
             }
